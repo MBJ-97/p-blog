67d1093fc92bdd9488c61162f2316fee732b5155
MBJ-97 <mehdiibeldjoudi@gmail.com>
2022-03-19 16:56:58 +0100

    fixed bug for google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,42 +4,28 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 import { ThemeProvider } from "next-themes";
 import Layout from "../components/Layout";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import Script from "next/script";
 import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
   return (
     <>
       <Script
-        strategy="afterInteractive"
+        id="google-analytics"
+        strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+
+      <Script id="google-analytics-keys" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${gtag.GA_TRACKING_ID}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <ThemeProvider attribute="class">
         <Layout>
           <Component {...pageProps} />
